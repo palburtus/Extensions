67aede53a7340f556d21e549883a13ad9455f3dd
Pat Alburtus <palburtus@webmd.net>
2024-08-28 08:16:20 -0400

    added String.IsNullEmptyOrWordNull

---
+++ Us.Alburt.Extensions/Us.Alburt.Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Us.Alburt.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmptyOrWordNull(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return true;
+
+            return value.ToLower().Trim() == "null";
+        }
+    }
+}
---
+++ Us.Alburt.Extensions/Us.Alburt.ExtensionsTests/StringExtensionsTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Us.Alburt.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Us.Alburt.Extensions.Tests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void IsNullOrEmptyOrWordNull_ValueIsNull_ReturnsTrue()
+        {
+            string? value = null;
+
+            bool actual = value!.IsNullOrEmptyOrWordNull();
+            
+            actual.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsNullOrEmptyOrWordNull_ValueIsStringDotEmpty_ReturnsTrue()
+        {
+            string value = string.Empty;
+
+            bool actual = value.IsNullOrEmptyOrWordNull();
+
+            actual.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsNullOrEmptyOrWordNull_ValueIsEmptyString_ReturnsTrue()
+        {
+            string value = "";
+
+            bool actual = value.IsNullOrEmptyOrWordNull();
+
+            actual.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsNullOrEmptyOrWordNull_ValueIsStringNull_ReturnsTrue()
+        {
+            string value = "null";
+
+            bool actual = value.IsNullOrEmptyOrWordNull();
+
+            actual.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void IsNullOrEmptyOrWordNull_ValueIsMultipleEmptySpaces_ReturnsFalse()
+        {
+            string value = "  ";
+
+            bool actual = value.IsNullOrEmptyOrWordNull();
+
+            actual.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void IsNullOrEmptyOrWordNull_ValueIsNotEmpty_ReturnsFalse()
+        {
+            string value = "test";
+
+            bool actual = value.IsNullOrEmptyOrWordNull();
+
+            actual.Should().BeFalse();
+        }
+    }
+}
